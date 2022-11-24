86aedf6569660ac1ba9525308ea1975e821fc53a
Gregoire Dechaine <gregdechaine@me.com>
2022-11-24 17:54:12 +0000

    Create Default Vuepress Structure

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,21 @@
-import { defineUserConfig } from 'vuepress'
+import { defaultTheme, defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
     lang: 'en-US',
     title: 'Hello Vuepress 2',
     description: 'Vuepress 2 Boilerplate',
+    theme: defaultTheme({
+        docsDir: 'docs',
+        repo: 'gregoiredechaine/vuepress-2-boilerplate-netlify-cms',
+        navbar: [
+            {
+                text: 'Vuepress 2 - Netlify CMS',
+                link: '/'
+            },
+            {
+                text: 'Admin',
+                link: 'admin/#/',
+            }
+        ]
+    })
 })
